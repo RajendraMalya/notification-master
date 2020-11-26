cd62a4fcefc2e03ad976041758a45cef2948cc45
Rajendra Malya <malya236@gmail.com>
2020-11-26 22:30:51 +0530

    lombok integration

--- src/main/java/notify/model/Message.java
+++ src/main/java/notify/model/Message.java
@@ -1,9 +1,12 @@
 package notify.model;
 
 import java.time.LocalDateTime;
+import lombok.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+@Getter
+@Setter
+@ToString
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Message {
     private long id;
@@ -17,65 +20,5 @@ public class Message {
         this.sentTime = LocalDateTime.now().toString();
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
-
-    public String getFrom() {
-        return from;
-    }
-
-    public void setFrom(String from) {
-        this.from = from;
-    }
-
-    public String getTo() {
-        return to;
-    }
-
-    public void setTo(String to) {
-        this.to = to;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getSentTime() {
-        return sentTime;
-    }
-
-    public void setSentTime(String sentTime) {
-        this.sentTime = sentTime;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "Message{" +
-                "id=" + id +
-                ", subject='" + subject + '\'' +
-                ", from='" + from + '\'' +
-                ", to='" + to + '\'' +
-                ", body='" + body + '\'' +
-                ", sentTime='" + sentTime + '\'' +
-                '}';
-    }
+    
 }
